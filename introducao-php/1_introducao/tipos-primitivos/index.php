23d00e01dcc361e92f521473f83db75d49865133
Lucas Fernandes <lucasfeitosa8@gmail.com>
2025-11-12 14:30:08 -0300

    (d0c): Realizando documentação sobre os conceitos de string

---
+++ introducao-php/1_introducao/tipos-primitivos/index.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Nesta seção de estudos iremos conhecer sobre os tipos de dados primitivos no PHP.
+ * 
+ * Quais são os tipos de dados primitivos? No PHP, existem quatro principais tipos de dados:
+ * 
+ * - String => Representa valores textuais, como frases, por exemplo.
+ * - Intenger => Representa números inteiros, que não possuem casa decimal.
+ * - Float => Representa números, porém, neste caso, irá representar números que possuem casa decimal.
+ * - Boolean => Representa valores booleanos. Ou seja, irá representar apenas dois valores: verdadeiro (true) ou falso (false).
+ * 
+ * Veremos abaixo de forma mais detalhada, os conceitos sobre cada um dos tipos citados.
+ */
+
+
+/**
+ * ### STRING ###
+ * 
+ * O tipo string, irá armazenar valores textuais. Para isto, necessitamos adicionar os valores entre aspas. 
+ * 
+ * Com o tipo de dado string, podemos armazenar como valor: nomes, frases, e quaisquer outros valores textuais que desejemos armazenar. Em alguns momentos, necessitaremos utilizar o tipo string para receber como valor para determinadas variáveis, não necessariamente valores textuais. Isto ocorre, por conta de questões específicas da lógica e da regra de negócio.
+ */
+
+// Exemplos de string
+$nome = "José";
+$fraseAleatoria = "O dia está muito bonito hoje.";
